90e004aac9a75e1b8fb4bedc5031263f701fce22
Mirko Cukich <mirkocukich@gmail.com>
2015-09-16 21:25:47 -0500

    Initial Commit

---
+++ WhatNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatNumber
+//
+//  Created by Mirko Cukich on 9/16/15.
+//  Copyright © 2015 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
